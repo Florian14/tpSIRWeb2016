d8e648e2dc0c7c041453981ad36bdc269f9613f7
Florian <florian14400@gmail.com>
2016-02-10 09:56:20 +0100

    modele vue ok

--- model.js
+++ model.js
@@ -1,17 +1,22 @@
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
 function Drawing(){
-    this.forms = new Array();
-    this.getForms = function() { return this.forms;}.bind(this);
+    this.shapes = new Array();
+
+    this.addShape = function(shape) { this.shapes.push(shape);}.bind(this);
+    this.getShapes = function() { return this.shapes;}.bind(this);
 };
 
-function Form(couleur, epaisseur){
+function Shape(couleur, epaisseur){
     this.couleur = couleur;
     this.epaisseur = epaisseur;
+	
+	this.getCouleur = function() { return this.couleur;}.bind(this);
+	this.getEpaisseur = function() { return this.epaisseur;}.bind(this);
 };
 
-function Line(couleur, epaisseur, x1, y1, x2, y2){
-    Form.call(this, couleur, epaisseur);
+function Line(x1, y1, x2, y2, epaisseur, couleur){
+    Shape.call(this, couleur, epaisseur);
     this.x1 = x1;
     this.y1 = y1;
     this.x2 = x2;
@@ -22,9 +27,10 @@ function Line(couleur, epaisseur, x1, y1, x2, y2){
     this.getFinalX = function() { return this.x2;}.bind(this);
     this.getFinalY = function() { return this.y2;}.bind(this);
 };
+Line.prototype = new Shape();
 
-function Rectangle(couleur, epaisseur, x1, y1, largeur, hauteur){
-    Form.call(this, couleur, epaisseur);
+function Rectangle(x1, y1, largeur, hauteur, epaisseur, couleur){
+    Shape.call(this, couleur, epaisseur);
     this.x1 = x1;
     this.y1 = y1;
     this.largeur = largeur;
@@ -32,6 +38,9 @@ function Rectangle(couleur, epaisseur, x1, y1, largeur, hauteur){
 
     this.getInitX = function() { return this.x1;}.bind(this);
     this.getInitY = function() { return this.y1;}.bind(this);
+    this.getLargeur = function() { return this.largeur;}.bind(this);
+    this.getHauteur = function() { return this.hauteur;}.bind(this);
     this.getFinalX = function() { return this.x1 + this.largeur;}.bind(this);
     this.getFinalY = function() { return this.y1 + this.hauteur;}.bind(this);
 };
+Rectangle.prototype = new Shape();
--- view.js
+++ view.js
@@ -1,13 +1,22 @@
 
 // Implémenter ici les fonctions paint à ajouter dans chacune des classes du modèle.
+Shape.prototype.paint = function(ctx) {
+    console.log("Shape.paint");
+    ctx.fillStyle = this.getCouleur();
+    ctx.fill();
+    ctx.lineWidth = this.getEpaisseur();
+}
+
 Rectangle.prototype.paint = function(ctx) {
-    //TODO Manager color
+    Shape.prototype.paint.call(this, ctx);
+    console.log("Rectangle.paint");
     ctx.rect(this.getInitX(), this.getInitY(), this.getFinalX(), this.getFinalY());
     ctx.stroke();
 };
 
 Line.prototype.paint = function(ctx) {
-    //TODO Manager color
+    Shape.prototype.paint.call(this, ctx);
+    console.log("Line.paint");
     ctx.beginPath();
     ctx.moveTo(this.getInitX(), this.getInitY());
     ctx.lineTo(this.getFinalX(), this.getFinalY());
@@ -17,10 +26,11 @@ Line.prototype.paint = function(ctx) {
 
 
 Drawing.prototype.paint = function(ctx) {
-    console.log(this.getForms());
+    console.log("Drawing.paint");
+    console.log(this.getShapes());
     ctx.fillStyle = '#F0F0F0'; // set canvas' background color
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    this.getForms().forEach(function(eltDuTableau) {
+    this.getShapes().forEach(function(eltDuTableau) {
         // now fill the canvas
         eltDuTableau.paint(ctx);
     });
